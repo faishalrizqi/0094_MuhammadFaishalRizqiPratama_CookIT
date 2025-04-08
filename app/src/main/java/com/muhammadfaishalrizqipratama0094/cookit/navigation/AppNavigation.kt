9bd16f29f12b4efc3a2dd901d86ff8a354f41527
Muhammad Faishal Rizqi Pratama <rizqifaishal03@gmail.com>
2025-04-08 12:07:04 +0700

    membuat AppNavigation

---
+++ app/src/main/java/com/muhammadfaishalrizqipratama0094/cookit/navigation/AppNavigation.kt
@@ -0,0 +1,41 @@
+package com.muhammadfaishalrizqipratama0094.cookit.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.muhammadfaishalrizqipratama0094.cookit.ui.screens.KategoriScreen
+import com.muhammadfaishalrizqipratama0094.cookit.ui.screens.HomeScreen
+import com.muhammadfaishalrizqipratama0094.cookit.ui.screens.ResepDetailScreen
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Categories : Screen("categories")
+    object ResepDetail : Screen("resep_detail/{resepId}") {
+        fun createRoute(resepId: Int) = "resep_detail/$resepId"
+    }
+}
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+        composable(Screen.Categories.route) {
+            KategoriScreen(navController = navController)
+        }
+        composable(
+            route = Screen.ResepDetail.route,
+            arguments = listOf(navArgument("recipeId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val resepId = backStackEntry.arguments?.getInt("recipeId") ?: 0
+            ResepDetailScreen(
+                resepId = resepId,
+                navController = navController
+            )
+        }
+    }
+}
